34753e296f6dec33fad8c2a0f15b5529cd498eda
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-09 10:56:04 +0900

    [chore] 라우팅 경로 정의

---
+++ src/pages/Message/CreateMessage.tsx
@@ -0,0 +1,5 @@
+const CreateMessage = () => {
+  return <>메세지 보내는 플로우</>;
+};
+
+export default CreateMessage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,80 @@
-import type { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([]);
+import Loading from '../components/common/Loading';
+
+const Login = lazy(() => import('../pages/Login'));
+const Signup = lazy(() => import('../pages/Signup'));
+const MyHome = lazy(() => import('../pages/MyHome'));
+const OthersHome = lazy(() => import('../pages/OthersHome'));
+const Message = lazy(() => import('../pages/Message/Message'));
+const MessageDetail = lazy(() => import('../pages/Message/MessageDetail'));
+const CreateMessage = lazy(() => import('../pages/Message/CreateMessage'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<Loading />}>
+        {/* TODO: 로그인 여부에 따라 본인 홈 or 로그인 화면으로 리다이렉션하도록 */}
+        <MyHome />
+      </Suspense>
+    ),
+  },
+  {
+    path: 'login',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: 'signup',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Signup />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/:userId',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <OthersHome />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/message/write',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <CreateMessage />
+      </Suspense>
+    ),
+  },
+  {
+    path: 'message',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Message />
+      </Suspense>
+    ),
+  },
+  {
+    path: 'message/:id',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <MessageDetail />
+      </Suspense>
+    ),
+  },
+
+  // TODO: Design not found page
+  {
+    path: '*',
+    element: <div>페이지를 찾을 수 없습니다 (404)</div>,
+  },
+]);
 
 export default router;
