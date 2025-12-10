bce870d580c1cf54b125e9be88d6655e4fca8026
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-10 10:00:54 +0900

    [feat] 가이드 UI 구현

--- src/pages/Guid.tsx
+++ src/pages/Guid.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useFunnel, Step } from '../components/common/Funnel';
-// import TextBubble from '../components/guid/TextBubble';
+import TextBubble from '../components/guid/TextBubble';
 
 const FUNNEL_STEPS = [1, 2, 3, 4];
 
@@ -12,7 +12,7 @@ const Guid = () => {
   const navigate = useNavigate();
 
   function handleNext() {
-    if (stepState === FUNNEL_STEPS[length - 1]) {
+    if (stepState === FUNNEL_STEPS[FUNNEL_STEPS.length - 1]) {
       navigate('/');
     } else setStepState(stepState + 1);
   }
@@ -24,19 +24,25 @@ const Guid = () => {
 
       <FunnelComponent>
         <Step name={1}>
-          <h2>1단계: 기본 정보 입력</h2>
+          <TextBubble type="none">재료를 모을 수 있어요 </TextBubble>
         </Step>
 
         <Step name={2}>
-          <h2>2단계: 결제 정보 입력</h2>
+          <TextBubble type="bottom">
+            재료를 모두 모으면 <br /> 눈사람을 만들 수 있어요!
+          </TextBubble>
         </Step>
 
         <Step name={3}>
-          <h2>3단계: 정보 검토</h2>
+          <TextBubble type="none">
+            만들어진 눈사람은 <br /> 여기서 확인할 수 있어요
+          </TextBubble>
         </Step>
 
         <Step name={4}>
-          <h2>4단계: 가입 완료!</h2>
+          <TextBubble type="bottom">
+            재료가 부족하다면 <br /> 친구에게 요청해보세요!
+          </TextBubble>
         </Step>
       </FunnelComponent>
     </div>
