80862a859534f2cfd4e7cea351112543f4b4a481
hyunw-kang <vpffp368@naver.com>
2025-12-10 14:43:52 +0900

    [feat] Home 하단 UI 구현

--- src/pages/MyHome.tsx
+++ src/pages/MyHome.tsx
@@ -3,6 +3,13 @@ import Button from '../components/common/Button';
 import homeGroupImg from '../assets/images/Home_Group.svg';
 import logo from "../assets/images/logo.svg";
 import letter from "../assets/images/letter.svg";
+import branchBImg from "../assets/images/branch_B.svg";
+import carrotBImg from "../assets/images/carrot_B.svg";
+import rockBImg from "../assets/images/rock_B.svg";
+import scarfBImg from "../assets/images/scarf_B.svg";
+import snowballBImg from "../assets/images/snowball_B.svg";
+import kakaoLinkImg from "../assets/images/KakaoLink.svg";
+import hyperLinkImg from "../assets/images/HyperLink.svg";
 
 const MyHome = () => {
   return (
@@ -24,11 +31,38 @@ const MyHome = () => {
             </h1>
           </div>
           <img src={homeGroupImg} alt="홈 그룹" />
+          <div className="my-home__ingredients">
+            <div className="my-home__ingredient-item">
+              <img src={branchBImg} alt="나뭇가지" />
+              <span className="my-home__ingredient-name">나뭇가지</span>
+            </div>
+            <div className="my-home__ingredient-item">
+              <img src={carrotBImg} alt="당근" />
+              <span className="my-home__ingredient-name">당근</span>
+            </div>
+            <div className="my-home__ingredient-item">
+              <img src={rockBImg} alt="돌" />
+              <span className="my-home__ingredient-name">돌</span>
+            </div>
+            <div className="my-home__ingredient-item">
+              <img src={snowballBImg} alt="눈뭉치" />
+              <span className="my-home__ingredient-name">눈뭉치</span>
+            </div>
+            <div className="my-home__ingredient-item">
+              <img src={scarfBImg} alt="목도리" />
+              <span className="my-home__ingredient-name">목도리</span>
+            </div>
+          </div>
         </div>
       </div>
 
       <div className="my-home__actions">
-        <Button type="large">눈사람 만들기</Button>
+        <Button type="large">재료를 모아주세요</Button>
+        <p className="my-home__share-text">친구에게 공유하고 재료 받기</p>
+        <div className="my-home__share-icons">
+          <img src={hyperLinkImg} alt="링크 공유" className="my-home__share-icon" />
+          <img src={kakaoLinkImg} alt="카카오톡 공유" className="my-home__share-icon" />
+        </div>
       </div>
     </div>
   );
