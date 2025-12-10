3b7a61ffdc358df243d80e45637417ad76ccd27e
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-10 09:55:52 +0900

    [feat] 배경 설정 및 단계별 동작 설정

--- src/components/guid/TextBubble.tsx
+++ src/components/guid/TextBubble.tsx
@@ -1,8 +1,14 @@
-const TextBubble = ({ type }: { type: 'top' | 'bottom' | 'none' }) => {
+const TextBubble = ({
+  type,
+  children,
+}: {
+  type: 'top' | 'bottom' | 'none';
+  children: React.ReactNode;
+}) => {
   return (
     <div className="text-bubble-wrapper">
       {type === 'top' && <div className="arrow-top"></div>}
-      <div className="text-bubble">재료를 모을 수 있어요</div>
+      <div className="text-bubble">{children}</div>
       {type === 'bottom' && <div className="arrow-bottom"></div>}
     </div>
   );
--- src/pages/Guid.tsx
+++ src/pages/Guid.tsx
@@ -1,40 +1,42 @@
-import { useFunnel, Step } from '../components/common/Funnel'; // useFunnel 추가 및 경로 수정
-import TextBubble from '../components/guid/TextBubble';
+import { useNavigate } from 'react-router-dom';
+import { useFunnel, Step } from '../components/common/Funnel';
+// import TextBubble from '../components/guid/TextBubble';
 
 const FUNNEL_STEPS = [1, 2, 3, 4];
 
 const Guid = () => {
-  const [FunnelComponent, setStepState] = useFunnel(
+  const [FunnelComponent, setStepState, stepState] = useFunnel(
     FUNNEL_STEPS,
     FUNNEL_STEPS[0],
   );
+  const navigate = useNavigate();
 
+  function handleNext() {
+    if (stepState === FUNNEL_STEPS[length - 1]) {
+      navigate('/');
+    } else setStepState(stepState + 1);
+  }
   return (
     <div>
-      <TextBubble type="top" />
-      <br />
-      <TextBubble type="bottom" />
-      <TextBubble type="none" />
+      <div className="guid-background" onClick={handleNext}>
+        <span>{stepState} / 4</span>
+      </div>
 
       <FunnelComponent>
         <Step name={1}>
           <h2>1단계: 기본 정보 입력</h2>
-          <button onClick={() => setStepState(2)}>결제 단계 (2)로 이동</button>
         </Step>
 
         <Step name={2}>
           <h2>2단계: 결제 정보 입력</h2>
-          <button onClick={() => setStepState(3)}>정보 검토 (3)로 이동</button>
         </Step>
 
         <Step name={3}>
           <h2>3단계: 정보 검토</h2>
-          <button onClick={() => setStepState(4)}>가입 완료 (4)로 이동</button>
         </Step>
 
         <Step name={4}>
           <h2>4단계: 가입 완료!</h2>
-          <p>모든 단계가 완료되었습니다.</p>
         </Step>
       </FunnelComponent>
     </div>
