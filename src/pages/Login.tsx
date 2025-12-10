981f36db81b8e35564f1abf3689ce69fe59d67fb
hyunw-kang <vpffp368@naver.com>
2025-12-10 16:04:03 +0900

    [feat] Onboard UI 세팅

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/common/Button";
+import snowman_v from "../assets/images/snowman_face_v.svg";
+import branch from "../assets/images/branch.svg";
+import carrot from "../assets/images/carrot.svg";
+import rock from "../assets/images/rock.svg";
+import scarf from "../assets/images/scarf.svg";
+import snowball from "../assets/images/snowball.svg";
+
 const Login = () => {
-  return <>로그인</>;
+  const navigate = useNavigate();
+
+  return (
+    <Button type = "large">로그인</Button>
+  )
 };
 
 export default Login;
