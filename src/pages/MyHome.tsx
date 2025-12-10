9756bca28edd6db4116bbe1cc39c089cb7572029
hyunw-kang <vpffp368@naver.com>
2025-12-10 10:54:18 +0900

    [feat] Home UI 컴포넌트 배치

--- src/pages/MyHome.tsx
+++ src/pages/MyHome.tsx
@@ -1,10 +1,34 @@
-import ComponentExample from '../components/ComponentExample';
+import '../style/home.scss';
+import Button from '../components/common/Button';
+import homeGroupImg from '../assets/images/Home_Group.svg';
+import logo from "../assets/images/logo.svg";
+import letter from "../assets/images/letter.svg";
 
 const MyHome = () => {
   return (
-    <div className="container">
-      홈화면 - 본인
-      <ComponentExample />
+    <div className="my-home">
+      <div className="my-home__header">
+        <div className="my-home__header-icon">
+          <img src = {logo} alt ="로고"/>
+        </div>
+        <div className="my-home__header-icon">
+          <img src = {letter} alt = "편지기록함"/>
+        </div>
+      </div>
+
+      <div className="my-home__content">
+        <div className="my-home__user-section">
+          <h1 className="my-home__user-name">박예진님</h1>
+        </div>
+
+        <div className="my-home__home-group">
+          <img src={homeGroupImg} alt="홈 그룹" />
+        </div>
+      </div>
+
+      <div className="my-home__actions">
+        <Button type="large">눈사람 만들기</Button>
+      </div>
     </div>
   );
 };
