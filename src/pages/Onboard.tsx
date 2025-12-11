14a6d369757389d2eeb71454c26af9d1db6c4695
hyunw-kang <vpffp368@naver.com>
2025-12-11 19:59:57 +0900

    [feat] Onboard UI 구현

--- src/pages/Onboard.tsx
+++ src/pages/Onboard.tsx
@@ -6,16 +6,42 @@ import carrot from "../assets/images/carrot.svg";
 import rock from "../assets/images/rock.svg";
 import scarf from "../assets/images/scarf.svg";
 import snowball from "../assets/images/snowball.svg";
+import '../style/pages/Onboard/onboard.scss';
 
 const Onboard = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <img src = {snowman_v} alt = "온보딩"/>
-      <Button type = "large" onClick={() => navigate('/Login')}>로그인</Button>
+    <div className="onboard">
+      <div className="onboard__container">
+        <div className="onboard__snowman">
+          <img src={snowman_v} alt="눈사람" className="onboard__image" />
+        </div>
+
+        <div className="onboard__materials">
+          <img src={snowball} alt="눈덩이" className="onboard__material" />
+          <img src={rock} alt="돌" className="onboard__material" />
+          <img src={branch} alt="나뭇가지" className="onboard__material" />
+        </div>
+
+        <div className="onboard__button">
+          <Button type="large" onClick={() => navigate('/Login')}>
+            로그인
+          </Button>
+        </div>
+
+        <div className="onboard__signup">
+          <span className="onboard__text">아직 회원이 아니라면? </span>
+          <span 
+            className="onboard__link" 
+            onClick={() => navigate('/Signup')}
+          >
+            회원가입하기
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Onboard;
