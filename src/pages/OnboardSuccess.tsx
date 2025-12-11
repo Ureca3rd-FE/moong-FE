9769829a3cc36eae72e3f39c057fcb2cfddb9933
hyunw-kang <vpffp368@naver.com>
2025-12-11 17:11:22 +0900

    [feat] OnboardSuccess UI 구현

--- src/pages/OnboardSuccess.tsx
+++ src/pages/OnboardSuccess.tsx
@@ -1,6 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/common/Button";
+import snowmanGood from "../assets/images/snowman_face_good.svg";
+import "../style/pages/OnboardSuccess/onboardSuccess.scss";
+
 const OnboardSuccess = () => {
-    return <>로그인/회원가입 성공</>;
-  };
-  
-  export default OnboardSuccess;
+  const navigate = useNavigate();
+
+  return (
+    <div className="onboard-success">
+      <img
+        className="onboard-success__image"
+        src={snowmanGood}
+        alt="가입 완료 눈사람"
+      />
+      <div className="onboard-success__title">가입 완료!</div>
+      <div className="onboard-success__subtitle">
+        지금 바로 로그인하고
+        <br />
+        마음을 뭉쳐보세요!
+      </div>
+      <div className="onboard-success__button">
+        <Button type="large" onClick={() => navigate("/")}>
+          로그인 하러 가기
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default OnboardSuccess;
   
