6765c451230e81c606104f4a6c9405c59bc9913b
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-12 09:39:13 +0900

    [feat] axios hook 생성

---
+++ src/hooks/api.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-underscore-dangle */
+import axios, { AxiosHeaders, type InternalAxiosRequestConfig } from 'axios';
+
+// API URL
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+let refreshTokenPromise: Promise<any> | null = null;
+
+// Axios 인스턴스 생성
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+// Request 인터셉터: 모든 요청에 Authorization 헤더를 추가
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    // 기존 headers를 기반으로 AxiosHeaders 객체 생성
+    const newHeaders = new AxiosHeaders(config.headers || {});
+
+    if (accessToken) {
+      newHeaders.set('Authorization', `Bearer ${accessToken}`);
+    }
+    if (refreshToken) {
+      newHeaders.set('Authorization_refresh', `Bearer ${refreshToken}`);
+    }
+
+    // config 객체를 직접 변경하지 않고, 새 객체를 만들어 반환
+    const modifiedConfig = { ...config, headers: newHeaders };
+
+    return modifiedConfig;
+  },
+  (error) => Promise.reject(error),
+);
+
+// Response 인터셉터: 401 Unauthorized 에러 처리
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const { newAccessToken, newRefreshToken } = await refreshTokenPromise;
+        refreshTokenPromise = null; // 완료 후 초기화
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        originalRequest.headers.Authorization_refresh = `Bearer ${newRefreshToken}`;
+        return api(originalRequest);
+      } catch (err) {
+        refreshTokenPromise = null;
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/';
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
