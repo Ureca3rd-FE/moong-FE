a0cb99e876a540ce131bb6a9a710e1e9fbfa61ae
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-12 16:52:07 +0900

    [feat] 메세지 리스트 구현

--- src/pages/Message/Message.tsx
+++ src/pages/Message/Message.tsx
@@ -1,19 +1,36 @@
 import { useState } from 'react';
 import Category from '../../components/message/Category';
+import { open, unopen } from '../../mock/messages';
 
 import Snowman from '../../components/message/Snowman';
+
+const THEME = ['red', 'green', 'yellow', 'silver', 'navy'];
 
 const Message = () => {
   const [tapState, setTapState] = useState('OPEN');
 
-  const className = 'message-list-' + (tapState === 'OPEN' ? 'open' : 'unopen');
+  const className =
+    'message-list-bg-' + (tapState === 'OPEN' ? 'open' : 'unopen');
 
   return (
     <>
       {/* TODO: 헤더 추가 */}
       <Category tapState={tapState} setTapState={setTapState} />
-      <div className={className}>
+      {/* <div className={className}>
         <Snowman color="red" name="강현우" />
+      </div> */}
+      <div className={className}>
+        <div className="message-list-wrapper">
+          {open.map((m) => {
+            return (
+              <Snowman
+                key={m.uuid}
+                color={THEME[m.themeId]}
+                name={m.nickname}
+              />
+            );
+          })}
+        </div>
       </div>
     </>
   );
