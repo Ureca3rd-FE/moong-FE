2be86bff241f6830ea20090a9c9c072f835eac7f
tray0244 <tray024400@gmail.com>
2025-12-12 09:55:18 +0900

    [fix] 헤더 컴포넌트 불필요한 내부 함수 제거 및 리팩토링

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,61 +2,39 @@ import type { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import '../../style/components/header/header.scss';
 
-import backIcon from '../../assets/icons/Back.svg'
-import closeIcon from '../../assets/icons/Close.svg'
+import Back from '../../assets/icons/Back.svg?react';
+import Close from '../../assets/icons/Close.svg?react';
 
 interface HeaderProps{
   back?: boolean | ReactNode;
   close?: boolean | ReactNode;
-  center?: string;
+  text?: string;
 }
 
-export const Header = ({back, center, close}: HeaderProps) =>{
+export const Header = ({back, text, close}: HeaderProps) =>{
   const navigate = useNavigate();
 
-  // 뒤로가기 
-  const Back = () => {
-    if(back === true){
-      return(
-        <button type="button" onClick={() => navigate(-1)} aria-label="뒤로가기">
-          <img src={backIcon} alt="뒤로가기" />
-        </button>
-      );
-    }
-    return back;
-  };
-
-  // 닫기
-  const Close = () => {
-    if(close === true){
-      return(
-        <button type="button" onClick={() => navigate('/')} aria-label="닫기">
-          <img src={closeIcon} alt="닫기" />
-        </button>
-      );
-    }
-    return close;
-  }
-
-  // 중앙 텍스트
-  const Center = () => {
-    if(center){
-      return <span>{center}</span>
-    }
-    return center;
-  }
-
-
-  return (
+  return(
     <header className="header">
-      <div className="header__back">{Back()}</div>
+      <div className="header__back">
+        {back &&
+          <button type="button" onClick={() => navigate(-1)} aria-label="뒤로가기">
+            <Back/>
+          </button>}
+      </div>
 
-      <div className="header__center">{Center()}</div>
+      <div className="header__center">
+        <span>{text}</span>
+      </div>
 
-      <div className="header__close">{Close()}</div>
-        
+      <div className="header__close">
+        {close &&
+          <button type="button" onClick={() => navigate('/')} aria-label="닫기">
+            <Close/>
+          </button>}
+      </div>
     </header>
-  );
+  )
 };
 
 export default Header;
