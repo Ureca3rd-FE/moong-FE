abdc20c125610199bf7c9d873e9d8fceb9afe19b
tray0244 <tray024400@gmail.com>
2025-12-16 12:56:32 +0900

    [fix] path name 소문자로 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -109,7 +109,7 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: 'selectTheme',
+    path: 'selecttheme',
     element: (
       <Suspense fallback={<Loading />}>
         <SelectTheme />
