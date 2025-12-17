99e336df659f98accf7150e936aea7e24f9dfe14
tray0244 <tray024400@gmail.com>
2025-12-17 14:11:33 +0900

    [feat] hook usePostMessage 구현

---
+++ src/hooks/usePostMessage.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import api from './api';
+
+interface MessageCreateRequest {
+    themeId: number;
+    message: string;
+    receivedNickname: string;
+    senderNickname: string;
+}
+
+export const usePostMessage = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const sendMessage = async (data: MessageCreateRequest) => {
+    setLoading(true);
+    setError(null);
+
+    try{
+        const response = await api.post('/api/messages', data);
+
+        return response.data;
+        } catch (err: any) {
+        const errorMessage = err.response?.data?.message || '메세지 전송 중 오류가 발생했습니다.';
+
+    setError(errorMessage);
+        throw new Error(errorMessage);
+        } finally{
+    setLoading(false);
+        }
+    }; 
+
+    return {sendMessage, loading, error};
+}
