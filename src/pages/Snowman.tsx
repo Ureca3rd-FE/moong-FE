47083304fc1ebae13dd9e4e585038ab7300ec66f
hyunw-kang <vpffp368@naver.com>
2025-12-17 14:46:22 +0900

    [feat] Snowman UI 구현

---
+++ src/pages/Snowman.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import SnowmanLetter from "../assets/images/snowman_full_letter.svg";
+import "../style/pages/Snowman/Snowman.scss";
+
+const Snowman = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate('/message');
+        },2000);
+        return () => {
+            clearTimeout(timer);
+        };
+    },[navigate]);
+    
+    return(
+        <div className = "snowman">
+            <img
+            className = "snowman_image"
+            src = {SnowmanLetter}
+            alt = "눈사람생성"
+            />
+            <div className = "snowman_title">눈사람이 편지를 가져왔어요!</div>
+        </div>
+    );
+};
+export default Snowman;
