0741559ebbd53894e193ffa1b45f8522278a1c7e
hyunw-kang <vpffp368@naver.com>
2025-12-18 13:38:12 +0900

    [feat] usePostSnowman 구현

---
+++ src/api/usePostSnowman.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import api from "../hooks/api";
+import { data } from "react-router-dom";
+
+interface SnowmanResponse {
+    message : string;
+}
+
+const postSnowmanApi = async (userId: number): Promise<SnowmanResponse> => {
+    const response = await api.post<SnowmanResponse>('/api/snowman', null, {
+        params: { userId }
+    });
+    return response.data;
+};
+
+export const usePostSnowman = () => {
+    const [error, setError] = useState <string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+
+    const makesnowman = async (userId : number) : Promise<boolean> => {
+        setLoading(true);
+        setError(null);
+        try{
+            const response = await postSnowmanApi(userId);
+            console.log('눈사람생성 성공', response.message);
+            return true;
+        }catch (err : any) {
+            const errorMessage = err?.response?.data.message || '눈사람 생성에 실패했습니다.';
+            setError(errorMessage);
+            return false;
+        }finally{
+            setLoading(false);
+        }
+    };
+    return {makesnowman,loading, error};
+}
+export default usePostSnowman;
